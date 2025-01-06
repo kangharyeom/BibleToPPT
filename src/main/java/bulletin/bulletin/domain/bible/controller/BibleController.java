3afef2f8b0c37c6a60106be824c6800bf99d7321
kangharyeom <kangharyeom@gmail.com>
2025-01-06 16:44:05 +0900

    Refactor: BibleController 프로퍼티 관리 및 exception 관리

--- src/main/java/bulletin/bulletin/domain/bible/controller/BibleController.java
+++ src/main/java/bulletin/bulletin/domain/bible/controller/BibleController.java
@@ -1,10 +1,11 @@
 package bulletin.bulletin.domain.bible.controller;
 
+import bulletin.bulletin.domain.bible.dto.BiblePPTDownPostDto;
 import bulletin.bulletin.domain.bible.service.BibleService;
 import bulletin.bulletin.domain.bible.entity.Bible;
-import dto.BiblePPTDownPostDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,40 +19,66 @@ import java.util.List;
 @RequestMapping("/api/bibles")
 public class BibleController {
     private final BibleService bibleService;
+    @Value("${custom.resource.path.use}")
+    private boolean customResourcePathUse;
+    @Value("${bible.file.path}")
+    private String bibleFilePath;
+    @Value("${classpath.bible.file.path}")
+    private String classpathBibleFilePath;
 
     @PostMapping
-    public ResponseEntity<String> biblePost () {
-        for(int i=1; i<67; i++){
-            String filePath = "C:\\Users\\PC\\Downloads\\bible\\bibletxt\\number\\"+i+".txt";
-            log.info("filePath:{}",filePath);
-            bibleService.insertTextFromFile(filePath);
+    public ResponseEntity<String> biblePost() {
+        String filePath;
+        try {
+            for (int i = 1; i < 67; i++) {
+                if (customResourcePathUse) {
+                    filePath = bibleFilePath;
+
+                } else {
+                    filePath = classpathBibleFilePath;
+
+                }
+                filePath += i + ".txt";
+                bibleService.insertTextFromClasspathFile(filePath);
+            }
+            log.info("BIBLE LIST CREATED");
+        } catch (Exception e) {
+            log.debug("BIBLE POST ERROR" + e);
+
+            return ResponseEntity.ok("Couldn't create Bible List");
         }
+
         return ResponseEntity.ok("success");
     }
 
     @PatchMapping
-    public ResponseEntity<String> changeShortNameToFullName(){
+    public ResponseEntity<String> changeShortNameToFullName() {
         bibleService.changeShortNameToFullName();
         return ResponseEntity.ok("success");
     }
 
-    @GetMapping("/powerpoint/down")
-    public ResponseEntity<List<List<Bible>>> getBible (@RequestBody List<BiblePPTDownPostDto> biblePPTDownPostDtoList) throws IOException {
+    @GetMapping("/powerpoint/download")
+    public ResponseEntity<?> getBible(@RequestBody List<BiblePPTDownPostDto> biblePPTDownPostDtoList) throws IOException {
 
         List<List<Bible>> biblesList = new ArrayList<>();
         List<Bible> bibles;
-        for (int i = 0; i < biblePPTDownPostDtoList.size(); i++) {
-            log.info("Dto:{}", biblePPTDownPostDtoList.get(i).toString());
-            bibles = bibleService.getBible(
-                    biblePPTDownPostDtoList.get(i).getTitle(),
-                    biblePPTDownPostDtoList.get(i).getStartChapter(),
-                    biblePPTDownPostDtoList.get(i).getStartVerse(),
-                    biblePPTDownPostDtoList.get(i).getEndChapter(),
-                    biblePPTDownPostDtoList.get(i).getEndVerse()
-            );
-            biblesList.add(bibles);
+        try {
+            for (BiblePPTDownPostDto biblePPTDownPostDto : biblePPTDownPostDtoList) {
+                log.info("Dto:{}", biblePPTDownPostDto.toString());
+                bibles = bibleService.getBible(
+                        biblePPTDownPostDto.getTitle(),
+                        biblePPTDownPostDto.getStartChapter(),
+                        biblePPTDownPostDto.getStartVerse(),
+                        biblePPTDownPostDto.getEndChapter(),
+                        biblePPTDownPostDto.getEndVerse()
+                );
+                biblesList.add(bibles);
+            }
+            bibleService.makePPT(biblesList);
+        } catch (Exception e) {
+            log.debug("getBible SYSTEM ERROR" + e);
+            return ResponseEntity.ok("SYSTEM ERROR");
         }
-        bibleService.makePPT(biblesList);
 
         return ResponseEntity.ok(biblesList);
     }
