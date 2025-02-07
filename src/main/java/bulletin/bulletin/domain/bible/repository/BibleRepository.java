fdf55ffdd16881e183753e0b7011002ba9c9650c
Kangharyeom <kangharyeom@gmail.com>
2025-02-07 17:25:47 +0900

    Dev to Main (#3)
    * Feat: C:// 에 있는 성경 txt 파일을 DB에 insert하는 로직 완성 (#1)
    
    - Bible Entity 추가
    - Bible Controller 추가
    - Bible Repository 추가
    - Bible Service 추가
    
    * Feat: PPT 다운로드 구현 완료
    
    * Refactor: makePPT 도형 추가
    
    * Refactor: classpass에 성경 파일 추가(개역 개정)
    
    * Refactor: 홈, ppt 다운로드 페이지 추가
    
    * Refactor: BibleController 프로퍼티 관리 및 exception 관리
    
    * Refactor: Dto 경로 수정
    
    * Refactor: BibleService 주요 메서드 상단으로 이동
    
    * Refactor: HomeController PPT 다운로드 페이지 추가
    
    * Refactor: 롬복 버전 구체화
    
    * Refactor: bible-download.html
    
    - 다운로드 페이지 추가(화면)
    - ppt post dto 세팅을 위한 최소한의 input box
    
    * Refactor: BibleController
    
    - getMapping에서 postMapping으로 수정
    
    * Refactor: BibleService
    
    - for each문으로 반복문 처리
    
    * Refactor: BulletinApplicationTests
    
    - 경로 수정
    
    * Refactor: BibleService
    
    - 성경 내용을 전달받아 도형에 삽입하도록 수정
    - 폰트 사이즈 변경할 수 있도록 수정
    
    * Refactor: BibleService
    
    - 성경 내용을 전달받아 도형에 삽입하도록 수정
    - 폰트 사이즈 변경할 수 있도록 수정

--- src/main/java/bulletin/bulletin/domain/bible/repository/BibleRepository.java
+++ src/main/java/bulletin/bulletin/domain/bible/repository/BibleRepository.java
@@ -8,17 +8,33 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface BibleRepository extends JpaRepository<Bible, Long> {
-    @Query(value = "SELECT * FROM bibles WHERE title = :title " +
-            "AND bible_id BETWEEN ( " +
-            "SELECT bible_id FROM bibles WHERE title = :title AND chapter = :startChapter AND verse = :startVerse) " +
-            "AND ( " +
-            "SELECT bible_id FROM bibles WHERE title = :title AND chapter = :endChapter AND verse = :endVerse) " +
-            "ORDER BY bible_id ASC", nativeQuery = true)
+    @Query(value =
+            "SELECT * FROM bibles " +
+                    "WHERE title = :title AND bible_id BETWEEN " +
+                    "   (SELECT bible_id FROM bibles WHERE title = :title AND chapter = :startChapter AND verse = :startVerse) " +
+                    "   AND " +
+                    "   (SELECT bible_id FROM bibles WHERE title = :title AND chapter = :endChapter AND verse = :endVerse) " +
+                    "ORDER BY bible_id ASC", nativeQuery = true)
     List<Bible> findByBiblePPTDownPostDto (
             @Param("title") String title,
             @Param("startChapter") String startChapter,
             @Param("startVerse") String startVerse,
             @Param("endChapter") String endChapter,
             @Param("endVerse") String endVerse
-            );
+    );
+
+    @Query(value = "UPDATE bibles SET title = :fullTitle WHERE title = :shortTitle", nativeQuery = true)
+    void changeShortNameToFullName(@Param("shortTitle") String shortTitle, @Param("fullTitle") String fullTitle);
+
+    @Query(value = "UPDATE bibles SET content = REGEXP_REPLACE(content, '<[^>]+> ', '') WHERE content REGEXP '<[^>]+>'", nativeQuery = true)
+    void deleteAllow();
+
+    @Query("SELECT DISTINCT b.chapter FROM Bible b WHERE b.title = :title ORDER BY b.chapter")
+    List<String> findDistinctChaptersByTitle(@Param("title") String title);
+
+    @Query("SELECT DISTINCT b.verse FROM Bible b WHERE b.title = :title AND b.chapter = :chapter ORDER BY b.verse")
+    List<String> findDistinctVersesByTitleAndChapter(
+            @Param("title") String title,
+            @Param("chapter") String chapter
+    );
 }
--- src/main/java/bulletin/bulletin/domain/bible/service/BibleService.java
+++ src/main/java/bulletin/bulletin/domain/bible/service/BibleService.java
@@ -1,14 +1,26 @@
 package bulletin.bulletin.domain.bible.service;
 
+import bulletin.bulletin.domain.bible.BibleFullName;
+import bulletin.bulletin.domain.bible.BibleShortName;
 import bulletin.bulletin.domain.bible.entity.Bible;
 import bulletin.bulletin.domain.bible.repository.BibleRepository;
-import dto.BiblePPTDownResponseDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.poi.sl.usermodel.ShapeType;
+import org.apache.poi.sl.usermodel.TextParagraph;
+import org.apache.poi.sl.usermodel.VerticalAlignment;
+import org.apache.poi.xslf.usermodel.*;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -21,15 +33,115 @@ import java.util.List;
 public class BibleService {
     private final BibleRepository bibleRepository;
 
-    @Transactional
-    public void insertTextFromFile(String filePath) {
+    public void makePPT(List<List<Bible>> biblesList) throws IOException {
+        // 저장 위치 설정
+        String homeDirectory = System.getProperty("user.home");
+        String downloadDirectory = homeDirectory + "/Downloads";
 
-        Path path = Paths.get(filePath);
+        // 파일 이름 설정
+        String filePath = downloadDirectory + "/output.pptx";
+
+        XMLSlideShow ppt = new XMLSlideShow();
+        XSLFSlideMaster defaultMaster = ppt.getSlideMasters().get(0);
+        XSLFSlideLayout titleLayout = defaultMaster.getLayout(SlideLayout.TITLE);
+
+        for (List<Bible> bibles : biblesList) {
+            for (Bible bible : bibles) {
+                // 도형 생성 (예: 직사각형)
+                XSLFSlide slide = ppt.createSlide(titleLayout);
+                XSLFAutoShape shape1 = slide.createAutoShape();
+                XSLFAutoShape shape2 = slide.createAutoShape();
+
+                // 도형 타입 설정 (직사각형)
+                shape1.setShapeType(ShapeType.ROUND_RECT);
+                shape2.setShapeType(ShapeType.ROUND_RECT);
+
+                // 도형 크기 및 위치 설정
+                shape1.setAnchor(new Rectangle2D.Double(10, 400, 700, 130));
+                shape2.setAnchor(new Rectangle2D.Double(10, 400, 180, 130));
+
+                // 도형 두께 설정
+                shape1.setLineWidth(6.0);
+                shape2.setLineWidth(6.0);
+                Color skyBlue = new Color(179, 244, 255); // RGB로 진한 파랑색
+
+                // 도형 배경색 설정
+                Color darkBlue = new Color(25, 80, 120); // RGB로 진한 파랑색
+                shape1.setFillColor(Color.WHITE);
+                shape2.setFillColor(darkBlue);
+
+                // 제목 텍스트를 포함할 도형
+                XSLFAutoShape titleShape = slide.createAutoShape();
+                titleShape.setShapeType(ShapeType.ROUND_RECT);
+                titleShape.setAnchor(new Rectangle2D.Double(10, 400, 180, 130));
+                titleShape.setLineWidth(6.0);
+
+                // 제목 텍스트 설정
+                XSLFTextParagraph titlePara = titleShape.addNewTextParagraph();
+                XSLFTextRun titleRun = titlePara.addNewTextRun();
+                titleRun.setText(bible.getTitle());
+                titlePara.setTextAlign(TextParagraph.TextAlign.CENTER);
+                titleShape.setVerticalAlignment(VerticalAlignment.MIDDLE);
+
+                // 내용 텍스트 도형
+                XSLFAutoShape contentShape = slide.createAutoShape();
+                contentShape.setAnchor(new Rectangle2D.Double(190, 400, 520, 130));
+
+                // 내용 텍스트 설정
+                XSLFTextParagraph contentPara = contentShape.addNewTextParagraph();
+                XSLFTextRun contentRun = contentPara.addNewTextRun();
+                contentRun.setText(bible.getContent());
+                contentPara.setTextAlign(TextParagraph.TextAlign.CENTER);
+                contentShape.setVerticalAlignment(VerticalAlignment.MIDDLE);
+
+                // 폰트 색상 설정 (선택사항)
+                titleRun.setFontColor(Color.BLACK);
+                contentRun.setFontColor(Color.BLACK);
+
+                // 제목 폰트 설정
+                titleRun.setFontSize(30.0);  // 포인트 단위
+                titleRun.setFontFamily("맑은 고딕");  // 폰트 패밀리 설정
+                titleRun.setBold(true);  // 굵게 설정
+
+                // 내용 폰트 설정
+                contentRun.setFontSize(30.0);  // 포인트 단위
+                contentRun.setFontFamily("맑은 고딕");  // 폰트 패밀리 설정
+                contentRun.setBold(true);  // 굵게 설정
+
+                // 슬라이드 색상 지정
+                Color backgroundColor = new Color(250, 80, 195);
+                slide.getBackground().setFillColor(backgroundColor);
+            }
+        }
+        // PPT 다운로드
+        FileOutputStream out = new FileOutputStream(filePath);
+        ppt.write(out);
+        log.info("PPT DOWN");
+    }
+
+    @Transactional
+    public void insertTextFromFile(String classpathFilePath) {
+        Path path = Paths.get(classpathFilePath);
         try {
-            // txt의 charset이 EUC-KR임
             Files.lines(path, Charset.forName("EUC-KR")).forEach(line -> {
                 columnDistinguish(line);
             });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Transactional
+    public void insertTextFromClasspathFile(String classpathFilePath) {
+        try {
+            Resource resource = new ClassPathResource(classpathFilePath);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream(), Charset.forName("EUC-KR")));
+
+            reader.lines().forEach(line -> {
+                columnDistinguish(line);
+            });
+
+            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -67,14 +179,13 @@ public class BibleService {
             } else if (Character.isDigit(ch)) {
                 chapter.append(ch);
             }
-            // 다른 경우에 대한 처리가 필요하다면 추가할 수 있습니다.
         }
         log.info("title:{}", title);
         log.info("chapter:{}", chapter);
 
         // content 추출
         // str[0]을 제외한 나머지가 content 입니다.
-        for(int j =1; j<str.length; j++){
+        for (int j = 1; j < str.length; j++) {
             content.append(str[j] + " ");
         }
         log.info("content:{}", content);
@@ -88,8 +199,34 @@ public class BibleService {
         bibleRepository.save(bible);
     }
 
+    public void changeShortNameToFullName() {
+        BibleShortName[] bibleShortName = BibleShortName.values();
+        BibleFullName[] bibleFullName = BibleFullName.values();
+
+        for (int i = 0; i < bibleShortName.length; i++) {
+            String shortName = String.valueOf(bibleShortName[i]);
+            String fullName = String.valueOf(bibleFullName[i]);
+            bibleRepository.changeShortNameToFullName(shortName, fullName);
+        }
+
+        // <> 안에있는 소제목 제거
+        bibleRepository.deleteAllow();
+    }
+
     @Transactional
-    public List<Bible> getBible(String title, String startChapter, String startVerse, String endChapter, String endVerse){
-    return bibleRepository.findByBiblePPTDownPostDto(title, startChapter, startVerse, endChapter, endVerse);
+    public List<Bible> getBible(String title, String startChapter, String startVerse, String endChapter, String endVerse) {
+        return bibleRepository.findByBiblePPTDownPostDto(title, startChapter, startVerse, endChapter, endVerse);
+    }
+
+    // 특정 성경 책의 전체 장 목록 조회
+    public List<String> getChaptersByBook(String bookName) {
+        return bibleRepository.findDistinctChaptersByTitle(bookName);
+    }
+
+    // 특정 성경 책과 장의 전체 절 목록 조회
+    public List<String> getVersesByBookAndChapter(String bookName, String chapter) {
+        return bibleRepository.findDistinctVersesByTitleAndChapter(bookName, chapter);
     }
 }
+
+
